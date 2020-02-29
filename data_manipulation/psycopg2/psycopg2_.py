3d9799a0934b28684300639dd03114b1656ea5bd
anonymous <shawn.coding.acc@gmail.com>
2020-02-29 10:45:59 +0800

    Add new functions to psycopg2_.py

--- data_manipulation/psycopg2/psycopg2_.py
+++ data_manipulation/psycopg2/psycopg2_.py
@@ -1,52 +1,57 @@
 import psycopg2
 
 
-def psy_pg_connect(host, database, user, password):
+def psy_pg_connect(host, database, username, password):
     """
     Set up a connection to the postgres server
 
     Parameters
     ----------
     host: str
+        Hostname
     database: str
-    user: str
+        Database name
+    username: str
+        Username
     password: str
+        Password
 
     Returns
     -------
     tuple
         (connection, cursor)
     """
-    connection_string = f"host={host} port=5432 dbname={database} user={user} password={password}"
-
+    connection_string = f"host={host} port=5432 dbname={database} username={username} password={password}"
     connection = psycopg2.connect(connection_string)
     print("PostgreSQL database connected ...")
 
-    '''create a cursor object'''
     cursor = connection.cursor()
-
+    print("Cursor object created ...")
     return connection, cursor
 
 
-def query_to_pandas(query, connection):
+def query_to_pandas(sql_query, connection):
     """
     Query result into Pandas dataframe
 
     Parameters
     ----------
-    query: str
+    sql_query: str
+        SQL query
     connection: str
+        Connection return by psycopg2
 
     Examples
     --------
-    >>> query = '''SELECT * FROM users;'''
+    >>> sql_query = '''SELECT * FROM users;'''
 
     Returns
     -------
     pandas.core.frame.DataFrame
+        SQL results in Pandas dataframe
     """
     import pandas as pd
-    return pd.io.sql.read_sql_query(query, connection)
+    return pd.io.sql.read_sql_query(sql_query, connection)
 
 
 if __name__ == "__main__":
