465bedba02e1d06b23186cfd843ccd0f50b51a99
anonymous <shawn.coding.acc@gmail.com>
2020-02-29 19:16:44 +0800

    Update data_manipulation

--- data_manipulation/__init__.py
+++ data_manipulation/__init__.py
@@ -1,8 +1,5 @@
+from data_manipulation import data_types, geopandas, pandas, psycopg2
 from ._version import get_versions
 
-__version__ = get_versions()['version']
-del get_versions
-
-from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
