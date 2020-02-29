a6ac95b0274d33036f226da13f0c6475498e0e1c
anonymous <shawn.coding.acc@gmail.com>
2020-02-29 17:18:48 +0800

    Update versioneer

--- data_manipulation/_version.py
+++ data_manipulation/_version.py
@@ -41,8 +41,8 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "v"
-    cfg.parentdir_prefix = "data-manipulation-"
+    cfg.tag_prefix = ""
+    cfg.parentdir_prefix = "data_manipulation-"
     cfg.versionfile_source = "data_manipulation/_version.py"
     cfg.verbose = False
     return cfg
