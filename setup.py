335266528f046bf70fcf05c4b6917a20aaf81c8a
anonymous <shawn.coding.acc@gmail.com>
2020-03-01 16:17:50 +0800

    Update required packages

--- setup.py
+++ setup.py
@@ -20,19 +20,24 @@ CLASSIFIERS = [
     "Topic :: Scientific/Engineering",
 ]
 
+DEPENDENCIES = [
+    "numpy", "pandas", "IPython", "psycopg2",
+]
+
 setup(
     author="Shawn Ng",
     author_email="shawn.coding.acc@gmail.com",
     classifiers=CLASSIFIERS,
     cmdclass=versioneer.get_cmdclass(),
     description="Powerful data manipulation functions",
+    install_requires=DEPENDENCIES,
     license="BSD-3",
     long_description=LONG_DESCRIPTION,
     name="data_manipulation",
     packages=find_packages(include=["data_manipulation", "data_manipulation.*"]),
     platforms="any",
-    test_suite='nose.collector',
-    tests_require=['nose'],
+    test_suite="nose.collector",
+    tests_require=["nose"],
     url="https://github.com/shawnngtq/data-manipulation",
     version=versioneer.get_version(),
 )
