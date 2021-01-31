840c62d1a172f0182f16341b92212f0fc0e5baf7
anonymous <shawn.coding.acc@gmail.com>
2021-01-31 18:03:19 +0800

    Update env

---
+++ install_packages.sh
@@ -0,0 +1,15 @@
+conda config --env --add channels conda-forge
+conda config --env --set channel_priority strict
+conda install python
+conda install beautifulsoup4
+conda install cookiecutter
+conda install jupyterlab notebook
+conda install matplotlib
+conda install pandas
+conda install psycopg2
+conda install pyspark
+conda install seaborn
+conda install sphinx
+conda install tqdm
+conda install twine
+conda install versioneer
